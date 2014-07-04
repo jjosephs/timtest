7bf7f25ef702a73d33e0eab2fd254d7d7d2b1fe9
Julian Josephs <josephs.julian@gmail.com>
2014-07-03 22:51:37 -0500

    added red border to form fields to highlight errors

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -11,16 +11,16 @@
     @endif
 
     {{ Form::open(array('route' => 'fizzbuzz', 'method' => 'POST')) }}
-        <div class="form-group">
+        <div class="form-group {{{ $errors->has('userName') ? 'has-error' : '' }}} ">
             <label for="userName">Name</label>
             <input type="text" class="form-control" id="userName" name="userName" placeholder="Enter name">
         </div>
-        <div class="form-group">
+        <div class="form-group {{{ $errors->has('userEmail') ? 'has-error' : '' }}} ">
             <label for="userEmail">Email address</label>
             <input type="email" class="form-control" id="userEmail" name="userEmail" placeholder="Enter email">
         </div>
-        <div class="form-group">
-            <label for="userDateOfBirth">Password</label>
+        <div class="form-group {{{ $errors->has('userDateOfBirth') ? 'has-error' : '' }}} ">
+            <label for="userDateOfBirth">Date of Birth</label>
             <input type="text" class="form-control" id="userDateOfBirth" name="userDateOfBirth" placeholder="MM/DD/YYYY">
         </div>
         <button type="submit" class="btn btn-default">Submit</button>
