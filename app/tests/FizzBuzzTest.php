2deaea455a045900c9c06ca5289ad4bf3afa09bf
Julian Josephs <josephs.julian@gmail.com>
2014-07-03 23:15:23 -0500

    Added new test cases
    * true if the form page returns an OK to a get request
    * true if the /fizzbuzz page redirects to the form page on GET request
    * true if the /fizzbuzz page has errors when post call is made with no variables

--- app/tests/FizzBuzzTest.php
+++ app/tests/FizzBuzzTest.php
@@ -2,16 +2,56 @@
 
 class FizzBuzzTest extends TestCase {
 
-	/**
-	 * A basic functional test where the fizzbuzz page should accept the 3 parameters and return OK
-	 *
-	 * @return void
-	 */
-	public function testFizzBuzzPage()
-	{
-		$crawler = $this->client->request('POST', '/fizzbuzz', array('userName' => 'Julian', 'userEmail' => 'Julian@test.com', 'userDateOfBirth' => '12/29/1990'));
+    /**
+     * A basic functional test where the fizzbuzz page should accept the 3 parameters and return OK
+     *
+     * @return void
+     */
+    public function testFizzBuzzPage()
+    {
+        $this->client->request('POST', '/fizzbuzz', array('userName' => 'Julian', 'userEmail' => 'Julian@test.com', 'userDateOfBirth' => '12/29/1990'));
 
-		$this->assertTrue($this->client->getResponse()->isOk());
-	}
+        $this->assertTrue($this->client->getResponse()->isOk());
+    }
+
+
+    /**
+     * A basic functional test where the fizzbuzz page return errors if no POST variables are set
+     *
+     * @return void
+     */
+    public function testFizzBuzzPageWithoutPostVariables()
+    {
+        $this->client->request('POST', '/fizzbuzz');
+
+        // Asserting the session has errors for a given key...
+        $this->assertSessionHasErrors('userEmail', 'userEmail', 'userDateOfBirth');
+    }
+
+
+    /**
+     * A basic functional test where the fizzbuzz form page return OK at a get request
+     *
+     * @return void
+     */
+    public function testFizzBuzzFormPage()
+    {
+        $this->client->request('GET', '/');
+
+        $this->assertTrue($this->client->getResponse()->isOk());
+    }
+
+
+    /**
+     * A basic functional test where the fizzbuzz form page redirects to home page on a GET request
+     *
+     * @return void
+     */
+    public function testFizzBuzzRedirect()
+    {
+        $this->client->request('GET', '/fizzbuzz');
+
+        $this->assertRedirectedToAction('HomeController@showWelcome');
+    }
 
 }
